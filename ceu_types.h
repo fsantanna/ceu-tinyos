b37b18f6fc491b5cb5481df27a89402cdf53a5cb
Francisco Sant'Anna <francisco.santanna@gmail.com>
2014-03-31 19:55:30 -0300

    (+) ceu_types.h

---
+++ ceu_types.h
@@ -0,0 +1,23 @@
+#ifndef CEU_TYPES_H
+#define CEU_TYPES_H
+
+#include "ceu_types.h"
+
+typedef unsigned int  word;
+typedef unsigned int  uint;
+typedef unsigned char byte;
+/*typedef unsigned char bool;*/
+
+typedef int64_t  s64;
+typedef uint64_t u64;
+typedef int32_t  s32;
+typedef uint32_t u32;
+typedef int16_t  s16;
+typedef uint16_t u16;
+typedef int8_t    s8;
+typedef uint8_t   u8;
+
+typedef float    f32;
+typedef double   f64;
+
+#endif
